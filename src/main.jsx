a80d8bbe6ee8cc0961ac5c83a04f9366777bc2d6
Luis Ramirez <lurami86@gmail.com>
2026-09-15 14:27:51 -0300

    Permitir selector de moneda dinamico sin conversion forzada

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,5 @@ import { createRoot } from "react-dom/client";
 import AuthGate from "./AuthGate";
 import "./styles.css";
 import "./logo-original.css";
-import "./currencyDisplay.js";
 
 createRoot(document.getElementById("root")).render(<AuthGate />);
